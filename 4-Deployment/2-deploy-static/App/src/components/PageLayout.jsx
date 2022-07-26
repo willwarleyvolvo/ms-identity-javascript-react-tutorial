bed26fc050922862d1fee4588e11150692522b88
willwarleyvolvo <107242341+willwarleyvolvo@users.noreply.github.com>
2022-07-26 23:44:05 +0300

    Update PageLayout.jsx

--- 4-Deployment/2-deploy-static/App/src/components/PageLayout.jsx
+++ 4-Deployment/2-deploy-static/App/src/components/PageLayout.jsx
@@ -13,14 +13,14 @@ export const PageLayout = (props) => {
         <>
             <NavigationBar />
             <br />
-            <h5><center>Welcome to the Microsoft Authentication Library For React Tutorial</center></h5>
+            <h5><center>Subscription migration</center></h5>
             <br />
             {props.children}
             <br />
             <AuthenticatedTemplate>
                 <footer>
-                    <center>How did we do? 
-                        <a href="https://forms.office.com/Pages/ResponsePage.aspx?id=v4j5cvGGr0GRqy180BHbR73pcsbpbxNJuZCMKN0lURpUMlRHSkc5U1NLUkxFNEtVN0dEOTFNQkdTWiQlQCN0PWcu" target="_blank"> Share your experience!</a>
+                    <center>Subscription assesment 
+                        <a href="https://forms.office.com/r/sSCK8Nnwng" target="_blank"> click</a>
                     </center>
                 </footer>
             </AuthenticatedTemplate>
